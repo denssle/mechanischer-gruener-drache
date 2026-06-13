b59b9ffaed53b1be9d05486dcf66f0bbbfef9baf
Dominik Hellweg <dominik.hellweg@protonmail.com>
2026-06-13 11:41:20 +0200

    register commands and slash

--- index.js
+++ index.js
@@ -66,11 +66,42 @@ client.on(
     }
 );
 
+client.on(Discord.Events.InteractionCreate, async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+
+    const command = client.commands.get(interaction.commandName);
+
+    if (!command) return;
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+    }
+});
 
 client.commands = new Collection();
 
 for (const command of commands) {
     client.commands.set(command.data.name, command);
 }
+
+import { REST, Routes } from "discord.js";
+
+const rest = new REST().setToken(config.BOT_TOKEN);
+
+const commandData = commands.map(cmd => cmd.data.toJSON());
+
+await rest.put(
+    Routes.applicationGuildCommands(
+        config.CLIENT_ID,
+        config.GUILD_ID
+    ),
+    {
+        body: commandData
+    }
+);
+
+console.log("Slash Commands registriert.");
 
 await client.login(config.BOT_TOKEN);
